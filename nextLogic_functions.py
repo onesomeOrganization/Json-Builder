a8b238fbdbbdf2767e9e0c27bb340b94a56031f8
Flora Latz <flora.latz@tum.de>
2023-02-15 15:01:36 +0100

    Value_option

--- nextLogic_functions.py
+++ nextLogic_functions.py
@@ -1,5 +1,5 @@
-
-def create_next_logic_option(id_base, count, answer_option_base):
+# TODO: rename next option
+def create_next_option(id_base, count, answer_option_base):
     next_logic_option = '''
                 {
                 "id": "%s-opt%s",
@@ -16,6 +16,37 @@ def create_next_logic_option(id_base, count, answer_option_base):
               }''' %(id_base, count, id_base, answer_option_base, count)
     return next_logic_option
 
+# TODO: Create value option
+def create_value_option(id_base):
+    value_option = '''
+                {
+                "id": "%s-opt1",
+                "order": 1,
+                "type": "VALUE_GTE",
+                "number": 1,
+                "count": null,
+                "secondNumber": null,
+                "secondCount": null,
+                "questionAnswerOptionId": null,
+                "secondQuestionAnswerOptionId": null,
+                "questionId": "XY",
+                "refQuestionId": null
+              },
+              {
+                "id": "%s-opt2",
+                "order": 2,
+                "type": "VALUE_LT",
+                "number": 1,
+                "count": null,
+                "secondNumber": null,
+                "secondCount": null,
+                "questionAnswerOptionId": null,
+                "secondQuestionAnswerOptionId": null,
+                "questionId": "XY",
+                "refQuestionId": null
+              }'''%(id_base, id_base)
+    return value_option
+
 def create_nextLogic_options(id_base, answer_option_base, next_logics):
     if next_logics == None:
         return ""
@@ -25,6 +56,8 @@ def create_nextLogic_options(id_base, answer_option_base, next_logics):
         if letter == 'N':
             if number != 0:
                 next_logics_block += ","
-            next_logics_block += create_next_logic_option(id_base, count, answer_option_base)
+            next_logics_block += create_next_option(id_base, count, answer_option_base)
         count+=1
+        if letter == 'V':
+            next_logics_block += create_value_option(id_base)
     return next_logics_block
--- question_block_functions.py
+++ question_block_functions.py
@@ -628,7 +628,7 @@ def create_question(question, id_base, count, write_beginning, contents, answer_
           ],
           "nextLogic": {
             "id": "%s",
-            "type": "VALUE",
+            "type": "%s",
             "count": null,
             "nextQuestionId": null,
             "prevQuestionId": null,
@@ -636,36 +636,11 @@ def create_question(question, id_base, count, write_beginning, contents, answer_
             "questionRefLogicId": null,
             "sessionId": null,
             "options": [
-              {
-                "id": "%s-opt1",
-                "order": 1,
-                "type": "VALUE_GTE",
-                "number": 1,
-                "count": null,
-                "secondNumber": null,
-                "secondCount": null,
-                "questionAnswerOptionId": null,
-                "secondQuestionAnswerOptionId": null,
-                "questionId": "XY",
-                "refQuestionId": null
-              },
-              {
-                "id": "%s-opt2",
-                "order": 2,
-                "type": "VALUE_LT",
-                "number": 1,
-                "count": null,
-                "secondNumber": null,
-                "secondCount": null,
-                "questionAnswerOptionId": null,
-                "secondQuestionAnswerOptionId": null,
-                "questionId": "XY",
-                "refQuestionId": null
-              }
+              %s
             ],
             "refAdaptions": []
           }
-        },''' % (id_base, "true" if count == 0 and write_beginning == True else "null", "true" if count == 0 and write_beginning == True else "null", id_base, id_base, id_base, create_content_block(id_base, 2, contents) ,id_base, get_content_length(contents), get_content_length(contents), id_base, get_content_length(contents), id_base, get_content_length(contents), id_base, get_content_length(contents), id_base, id_base, id_base),
+        },''' % (id_base, "true" if count == 0 and write_beginning == True else "null", "true" if count == 0 and write_beginning == True else "null", id_base, id_base, id_base, create_content_block(id_base, 2, contents) ,id_base, get_content_length(contents), get_content_length(contents), id_base, get_content_length(contents), id_base, get_content_length(contents), id_base, get_content_length(contents), id_base, next_logic_type, create_nextLogic_options(id_base, get_content_length(contents), next_logics)),
         "ITEM_LIST_EXPANDABLE": '''
         {
           "id": "%s",
